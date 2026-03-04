a330649bcbde1c3ed840e30ed1b6dd6095aa4a48
Orange Services <torsten.seurig@orange-services.net>
2026-03-04 10:42:32 +0100

    Snapshot vor System-Log Feature

--- .history/vite.config_20260303143345.js
+++ .history/vite.config_20260303143345.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin'
 import fs from 'fs'
 
 export default defineConfig({
+    server: {
+        https: {
+            key: fs.readFileSync('C:/laragon/etc/ssl/laragon.key'),
+            cert: fs.readFileSync('C:/laragon/etc/ssl/laragon.crt'),
+        },
+        host: 'os-tools.test',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
