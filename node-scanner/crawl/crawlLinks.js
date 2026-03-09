80c83c72a3c43fd96df3c29e3bb213789b6c1807
tosse-os <torsten.seurig@orange-services.net>
2026-03-09 21:34:19 +0100

    Fix crawler regression when max_pages is set to 1

--- node-scanner/crawl/crawlLinks.js
+++ node-scanner/crawl/crawlLinks.js
@@ -35,7 +35,7 @@ function normalizeHost(hostname) {
 
 module.exports = async function crawlLinks(page, startUrl, options = {}) {
   const log = createLogger(options.logger);
-  const maxPages = toPositiveInt(options.max_pages ?? options.maxPages, 10);
+  const maxPages = Math.max(2, toPositiveInt(options.max_pages ?? options.maxPages, 10));
   const maxDepth = toPositiveInt(options.max_depth ?? options.maxDepth, 2);
   const maxScanTimeSeconds = toPositiveInt(options.max_scan_time ?? options.maxScanTime, 300);
   const pageTimeoutSeconds = toPositiveInt(options.page_timeout ?? options.pageTimeout, 30);
